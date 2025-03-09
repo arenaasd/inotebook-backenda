4133db7791e04f5359108634b40c65789a1acb32
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-10 03:15:04 +0500

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose');
+const debug = require('debug')('development:mongoose');
 
+// Use the environment variable for MongoDB URI
+const mongoUri = process.env.MONGODB_URI;
 
-const connectToDB = () =>{
-  try {
-       const con =  await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.log("Error connecting to MongoDB:", error);
-        
-    }
-}
+mongoose.connect(mongoUri)
+    .then(() => {
+        debug("Connected to MongoDB Atlas");
 
-module.exports = connectToDB;
+        // Test inserting a document into a collection
+        const testSchema = new mongoose.Schema({ name: String });
+        const TestModel = mongoose.model('Test', testSchema);
+
+        // Insert a document
+        const testDocument = new TestModel({ name: 'Test User' });
+
+        testDocument.save()
+            .then(() => {
+                debug("Document saved successfully!");
+            })
+            .catch((err) => {
+                debug("Error saving document:", err);
+            });
+    })
+    .catch((err) => {
+        debug("Error connecting to MongoDB:", err);
+    });
+
+module.exports = mongoose.connection;
