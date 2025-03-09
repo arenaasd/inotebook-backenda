44fd70bc40529fd69c5e0d045c6b19a7d47cf1bf
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-10 03:03:22 +0500

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 
 const connectToDB = () =>{
-    mongoose.connect('mongodb://localhost:27017/inotebook').then(() => {
-        console.log('Connected to DB');
-    }).catch((err) => {
-        console.error('Error connecting to DB:', err);
-    });
+  try {
+       const con =  await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+        
+    }
 }
 
 module.exports = connectToDB;
