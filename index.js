8c13780faa9cabe8eb13cd92f02f7566d38c29f0
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-10 06:20:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const cors = require('cors');
 connectToDB(); // Call the function here
 
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: 'https://inotebook-bay-xi.vercel.app', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
+}));
 app.use('/api/auth', auth);
 app.use('/api/notes', notes);
 
