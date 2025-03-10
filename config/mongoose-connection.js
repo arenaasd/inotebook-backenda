6ea12688e33e3fcd6aa5f6f12a0128174c0fa339
Arsalankhan1218 <90974742+arenaasd@users.noreply.github.com>
2025-03-10 06:03:49 +0500

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://akgamerz397:nadanak420@cluster0.rcjgs.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0
-"
-const connectToDB = async () =>{
+// Remove the newline character at the end of the URI
+const uri = "mongodb+srv://akgamerz397:nadanak420@cluster0.rcjgs.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0";
+
+const connectToDB = async () => {
    try {
-       const con =  await mongoose.connect(uri);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.log("Error connecting to MongoDB:", error);
-        
-    }
+       const con = await mongoose.connect(uri);
+       console.log("Connected to MongoDB");
+   } catch (error) {
+       console.log("Error connecting to MongoDB:", error);
+   }
 }
 
 module.exports = connectToDB;
